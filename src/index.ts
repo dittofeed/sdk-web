d19d3c191265cca5da9ff1d96b8921d059cd0f5c
Max Gurewitz <max@dittofeed.com>
2023-08-03 17:12:22 -0700

    docs

--- src/index.ts
+++ src/index.ts
@@ -20,20 +20,47 @@ export type TimeoutHandle = ReturnType<typeof setTimeout>;
  * @example
  * import { DittofeedSdk } from '@dittofeed/sdk-web';
  *
- * // Initialize the SDK, the writeKey found in your Dittofeed dashboard
- * // settings.
+ * // Initialize the sdk with a writeKey, which is used to identify your
+ * // workspace. This key can be found at
+ * // https://dittofeed.com/dashboard/settings
  * await DittofeedSdk.init({
- *   writeKey: '...',
+ *   writeKey: "Basic abcdefg...",
  * });
  *
- * // Send events to Dittofeed.
+ * // Lets you tie a user to their actions and record traits about them. It
+ * // includes a unique User ID and any optional traits you know about the
+ * // user, like their email, name, and more.
  * DittofeedSdk.identify({
- *   userId: '123',
+ *   userId: "123",
  *   traits: {
- *     firstName: 'John',
- *     email: 'john@email.com'
- *  }
+ *     email: "john@email.com",
+ *     firstName: "John"
+ *   },
  * });
+ *
+ * // The track call is how you record any actions your users perform, along
+ * // with any properties that describe the action.
+ * DittofeedSdk.track({
+ *   userId: "123",
+ *   event: "Made Purchase",
+ *   properties: {
+ *     itemId: "abc",
+ *   },
+ * });
+ *
+ * // Lets you record whenever a user sees a screen, the mobile equivalent of
+ * // page, in your mobile app, along with any properties about the screen.
+ * DittofeedSdk.screen({
+ *   userId: "123",
+ *   name: "Recipe Screen",
+ *   properties: {
+ *     recipeType: "Soup",
+ *   },
+ * });
+ *
+ * // Ensures that asynchronously submitted events are flushed synchronously
+ * // to Dittofeed's API.
+ * await DittofeedSdk.flush();
  */
 export class DittofeedSdk {
   private static instance: DittofeedSdk;
@@ -42,8 +69,25 @@ export class DittofeedSdk {
   static async init(initParams: InitParamsDataBase): Promise<DittofeedSdk> {
     const baseSdk = new DittofeedSdkBase({
       uuid: () => uuidv4(),
-      issueRequest: async (data, params) => {
-        return;
+      issueRequest: async (
+        data,
+        { host = "https://dittofeed.com", writeKey }
+      ) => {
+        const url = `${host}/api/public/apps/batch`;
+        const headers = {
+          authorization: writeKey,
+          "Content-Type": "application/json",
+        };
+
+        const response = await fetch(url, {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
       },
       setTimeout,
       clearTimeout,
